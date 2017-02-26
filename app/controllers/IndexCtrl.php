ec7d7d8477a12c42c26889ca5af3c4b57c737e1f
JP <jpc.araujoti@gmail.com>
2017-02-25 23:13:29 -0300

    Initial commit

---
+++ app/controllers/IndexCtrl.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace app\controllers;
+
+
+class IndexCtrl
+{
+
+    /***Actions***/
+
+    public function index(){
+        echo "Route: Index";
+    }
+
+    public function contact(){
+        echo "Route: Contact";
+    }
+}
